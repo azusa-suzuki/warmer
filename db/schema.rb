be6ba27bf57871e8d1a108bbb2984f837631089b
EC2 Default User <ec2-user@ip-172-31-39-134.ap-northeast-1.compute.internal>
2020-11-20 13:49:28 +0000

    Update devise

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,11 @@ ActiveRecord::Schema.define(version: 2020_11_15_111441) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
+    t.string "name", null: false
+    t.string "name_kana", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "phone", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
